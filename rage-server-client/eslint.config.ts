5784afaf81e110d8b333d3c38a7af1dc9cea5de4
pimpmfl <danielpopescu934@gmail.com>
2025-04-23 22:10:41 +0200

    chore: remove unnecessary dependencies

--- rage-server-client/eslint.config.ts
+++ rage-server-client/eslint.config.ts
@@ -1,11 +1,9 @@
 // @ts-check
 
-import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import prettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default tseslint.config(
-  eslint.configs.recommended,
   tseslint.configs.strict,
   tseslint.configs.stylistic,
   prettierRecommended,
